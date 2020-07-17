38afe2f19b8538181f0e763ed5cba55b486f655c
huycong2798 <45420711+huycong2798@users.noreply.github.com>
2020-07-17 17:59:37 +0700

    feat: add katalon n times  try

---
+++ Test Suites/BasicTestSuite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>BasicTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ac84e06d-10b3-49ce-8243-ec5f0df8e63d</testSuiteGuid>
+</TestSuiteEntity>
